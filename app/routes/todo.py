526ffeab87fef57a9845bc06197a1e9a3d23bcb7
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-08-20 11:41:42 -0300

    Criando a rota para cadastro de todo

--- app/routes/todo.py
+++ app/routes/todo.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, current_app, jsonify, request
 from app.models import Todo
 from app.serealizer import TodoSchema
 
@@ -10,3 +10,15 @@ bp_todos = Blueprint("todos", __name__)
 def list_todos():
     result = Todo.query.all()
     return TodoSchema(many=True).jsonify(result), 200
+
+
+@bp_todos.route("/register", methods=["POST"])
+def register_todo():
+    todo_schema = TodoSchema()
+
+    todo = todo_schema.load(request.json)
+
+    current_app.db.session.add(todo)
+    current_app.db.session.commit()
+
+    return todo_schema.jsonify(todo), 201
